91d626c1966dcc76633cb822a7c29fac81b9cc9f
JoshGutman <jng86@nau.edu>
2016-12-06 02:33:53 -0700

    Fixed incorrect message_length variable and >56 byte input size

--- sha.py
+++ sha.py
@@ -4,17 +4,17 @@ from digest import digest
 
 def sha(message):
 
-    # 64-bit binary representation of the length of the original message
-    message_length = bin(len(message))[2:].zfill(64)
+    # 64-bit binary string of the amount of bits in the original message
+    message_length = bin(len(message) * 8)[2:].zfill(64)
 
 
     # Convert inputted message to binary
     bin_message = _ascii_to_bin(message)
 
     
-    # Append 1 to binary number
+    # Append 1 to binary message
     bin_message += "1"
-
+    
 
     # Pad binary number with zeros until its length is congruent to 448 mod 512
     amount_zeros = 448 - (len(bin_message) % 512)
@@ -40,13 +40,20 @@ def sha(message):
 
 
     # Digest all chunks and return the result
-    hashes = []
+    h_list = ["01100111010001010010001100000001", "11101111110011011010101110001001",
+              "10011000101110101101110011111110", "00010000001100100101010001110110",
+              "11000011110100101110000111110000"]
     for wordlist in wordlist_list:
-        hh = digest(wordlist)
-        hashes.append(hh)
+        h_list = digest(wordlist, h_list)
 
 
-    return hashes
+    # Create final hash by combine the parts in h_list
+    hh = ""
+    for h in h_list:
+        hh += hex(int(h,2))[2:]
+
+    
+    return hh
 
 
         
